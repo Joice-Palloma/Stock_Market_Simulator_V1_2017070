bbff88e6422b77194c456e66f3e7f8bcf8cac6d0
Joice-Palloma <joicepgm@gmail.com>
2020-05-11 21:10:38 +0100

    Menu display

--- Stock Market Simulator V1/src/Main.java
+++ Stock Market Simulator V1/src/Main.java
@@ -3,6 +3,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+		
+		
+		 String displayMenu = "\nCompany with the highest capital\n" +
+	                "Company with the lowest capital\n" +
+	                "Investor with the highest number of shares\n" +
+	                "Investor with the lowest number of shares\n";
+		 
+		  System.out.println(displayMenu);
+
+	
 
 	}
 
